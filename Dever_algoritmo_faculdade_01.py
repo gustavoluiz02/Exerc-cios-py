e4afa3923dce5418812229be7e4e454438c98158
gustavoluiz02 <gustavoluizsilva2002@gmail.com>
2021-03-21 16:01:51 -0300

    Adicionando exercícios

---
+++ Dever_algoritmo_faculdade_01.py
@@ -0,0 +1,4 @@
+raio = float(input('Qual o tamanho do raio?'))
+pi = 3.14
+area = pi*raio*raio
+print ( 'A área do circulo é de:', area)
---
+++ potencia.py
@@ -0,0 +1,12 @@
+print("base ^ expoente:")
+base=int(input("Base: "))
+expoente=int(input("Expoente: "))
+
+potencia=1
+count=1
+
+while count <= expoente:
+    potencia *= base
+    count +=1
+
+print(base,"^",expoente,"=",potencia)
